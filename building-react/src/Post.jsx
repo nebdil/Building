a8d3771daec0bdcdf2785c44cafc30e005ec1d7c
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-16 21:02:47 +0000

    imports to React components

---
+++ building-react/src/Post.jsx
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import Reply from './Reply.jsx';
+import Like from './Like.jsx';
+import Tag from './Tag.jsx';
---
+++ building-react/src/User.jsx
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import Post from './Post.jsx';
+import Reply from './Reply.jsx';
