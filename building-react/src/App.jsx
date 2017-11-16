ed35e1a5db78c550ee770659c2685f49b2d06821
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-16 20:55:52 +0000

    initiates React component setup

---
+++ building-react/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Building from './Building.jsx';
+import User from './User.jsx';
+import Post from './Post.jsx';
+import Reply from './Reply.jsx';
+import Like from './Like.jsx';
+import Tag from './Tag.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Emmi Land</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
