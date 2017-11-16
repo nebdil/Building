217dd1ca8c0926b704fb33f242fc3fae04f11b7e
Dilan <dilannebioglu@gmail.com>
2017-11-16 19:22:15 +0000

    routes done

--- building_rails/config/routes.rb
+++ building_rails/config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root to: 'buildings#show'
+
+  # NESTED ROUTES
+
+  resources :buildings, only: [:index, :create, :show, :new] do
+    resources :users, only: [:create, :show, :edit, :update, :index]
+    resources :posts do
+      resources :likes, only: [:index, :create, :destroy]
+      resources :replies, only: [:index, :new, :create, :destroy]
+    end
+  end
+
+  # REGISTER ROUTES
+
+  get '/register' => 'users#new'
+
+  # LOGIN ROUTES
+
+  post '/users' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
