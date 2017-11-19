0154e2c84f88eebe7531738fd3e43b0f1d26ef11
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-19 21:29:32 +0000

    gets reply data

--- building_rails/app/controllers/users_controller.rb
+++ building_rails/app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
     @user = @building.users.find_by(id: params[:id])
     @posts = @user.posts.all.order('posts.id DESC')
     post_arr = @posts.map do |po|
-      pp po.likes
       result = po.attributes
       result[:username] = po.user.username
       result[:reply] = po.replies
@@ -21,6 +20,16 @@ class UsersController < ApplicationController
       result[:tags] = po.tags
       result
     end
+    @posts_user_replied_to = @user.posts.all.joins(:replies).includes(:replies).where(replies: {user_id: params[:id]}).order('posts.id DESC')
+    reply_arr = @posts_user_replied_to.map do |pos|
+      result = pos.attributes
+      result[:username] = pos.user.username
+      result[:reply] = pos.replies
+      result[:like] = pos.likes
+      result[:tags] = pos.tags
+      result
+    end
+    post_arr.push({posts_user_replied_to: reply_arr})
     render json: post_arr
   end
   def update
