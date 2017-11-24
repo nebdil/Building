3059585d99f3fb2402b8f56a4b9238ef1f160c0c
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-24 04:18:02 +0000

    adds primitive styling to send post

--- building-react/src/CreatePost.jsx
+++ building-react/src/CreatePost.jsx
@@ -5,13 +5,11 @@ export default class CreatePost extends Component {
   render() {
 
     return (
-      <form onSubmit={this.props.handleNewPost}>
+      <form className="send-post" onSubmit={this.props.handleNewPost}>
         <label htmlFor="post_content">
-          New Post:
         </label>
         <input type="text" name="post_content" placeholder="Your post" onChange={this.props.handleContent} />
         <label htmlFor="tag_name">
-          New Tag:
         </label>
         <input type="text" name="tag_name" placeholder="Your tag" onChange={this.props.handleTag} />
         <button>Create post!</button>
