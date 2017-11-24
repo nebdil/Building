7ba7a653269158c5ca64a438991d2b495f14e0b8
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-24 16:22:09 +0000

    completes the building layout

--- building-react/src/CreatePost.jsx
+++ building-react/src/CreatePost.jsx
@@ -8,11 +8,12 @@ export default class CreatePost extends Component {
       <form className="send-post" onSubmit={this.props.handleNewPost}>
         <label htmlFor="post_content">
         </label>
-        <input type="text" name="post_content" placeholder="Your post" onChange={this.props.handleContent} />
+        <textarea className="post-container" name="post_content" placeholder="What's going on?" onChange={this.props.handleContent} />
         <label htmlFor="tag_name">
         </label>
-        <input type="text" name="tag_name" placeholder="Your tag" onChange={this.props.handleTag} />
+        <input className="tag-container" type="text" name="tag_name" placeholder="Add a tag!" onChange={this.props.handleTag} />
         <button>Create post!</button>
+        <hr />
       </form>
     )
   }
