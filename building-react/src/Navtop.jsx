61d0ee8a075d6818073a1d655aae53726817b90a
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-25 04:59:44 +0000

    adds logo to the navbar

--- building-react/src/Navtop.jsx
+++ building-react/src/Navtop.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
-import {Navbar, Nav, NavItem} from 'react-bootstrap'
+import {Navbar, Nav, NavItem, Grid, Row, Col} from 'react-bootstrap'
 
 export default class Navtop extends Component {
   constructor(props) {
@@ -11,30 +11,41 @@ export default class Navtop extends Component {
   render() {
     return (
       <Navbar id="navbar">
-        <Nav>
-          <div class="menu-item" id="left-most-nav">
-            <NavItem><Link to={'/buildings/1/users/1'}>My Profile</Link></NavItem>
-            <div class="color-div"></div>
-          </div>
-        </Nav>
-        <Navbar.Brand>
-          <div id="logo">
-            <a href="#">Static Building Logo</a>
-          </div>
-        </Navbar.Brand>
-        <Nav>
-          <div class="menu-item" id="second-right-most-nav">
-            <NavItem><p>Signed in as 1 - in 3601</p></NavItem>
-            <div class="color-div"></div>
-          </div>
-        </Nav>
-        <Nav>
-          <div class="menu-item" id="right-most-nav">
-            <NavItem onClick={this._handleLogout}><Link to={'/logout'}>Log Out</Link></NavItem>
-            <div class="color-div"></div>
-          </div>
-
-        </Nav>
+        <Grid>
+          <Row>
+            <Col md={3}>
+              <Navbar.Brand>
+                <div id="logo">
+                  <Link to={'#'}><img src={"/building-coral.png"} alt="building" width="30"/><p>BUILDING</p></Link>                  
+                </div>
+              </Navbar.Brand>
+            </Col>
+            <Col md={3}>
+              <Nav>
+                <div class="menu-item">
+                  <NavItem><p>Signed in as 1 - in 3601</p></NavItem>
+                  <div class="color-div"></div>
+                </div>
+              </Nav>
+            </Col>
+            <Col md={3}>
+              <Nav>
+                <div class="menu-item">
+                  <NavItem><Link to={'/buildings/1/users/1'}>My Profile</Link></NavItem>
+                  <div class="color-div"></div>
+                </div>
+              </Nav>
+            </Col>
+            <Col md={3}>
+              <Nav>
+                <div class="menu-item" id="right-most-nav">
+                  <NavItem onClick={this._handleLogout}><Link to={'/logout'}>Log Out</Link></NavItem>
+                  <div class="color-div"></div>
+                </div>
+              </Nav>
+            </Col>
+          </Row>
+        </Grid>
       </Navbar>
     )
   }
