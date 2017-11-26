82dab94a315957b6e5d0d37982acd2bf40c3fec1
mcemolcusenler <cem.olcusenler@gmail.com>
2017-11-26 02:47:26 +0000

    adds fonts and changes the landing page

--- building-react/src/About.jsx
+++ building-react/src/About.jsx
@@ -27,17 +27,15 @@ export default class About extends Component {
     console.log(this.state.proPS)
     console.log(this.props.propS.history)
     return(
-
-
     <Jumbotron>
       <Col md={3}></Col>
       <Col md={6}>
-        <Row><h1>Welcome to Building!</h1></Row>
-        <Row><h3>... where you can connect with your neighbors.</h3></Row>
+        <Row><h1>BUILDING</h1></Row>
+        <Row><h2>We connect you with your neighbors.</h2></Row>
         <Row>
           <Col md={4}></Col>
           <Col md={4}>
-            <Button bsStyle="primary" className="login-btn" onClick={this._handleLogin}>
+            <Button bsStyle="primary" id="login-btn" className="login-btn" onClick={this._handleLogin}>
               Login
             </Button>
             <Button bsStyle="primary" className="login-btn" onClick={this._handleRegister} >
