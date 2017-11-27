6c01d6ba65c70217744a0902c7b1ab03f5f577cd
nebdil <dilannebioglu@gmail.com>
2017-11-27 12:05:37 -0500

    likes debugging

--- building_rails/app/controllers/likes_controller.rb
+++ building_rails/app/controllers/likes_controller.rb
@@ -15,62 +15,105 @@ class LikesController < ApplicationController
     puts '@user[:id]'
     puts @user[:id]
 
-    like = Like.find_by(post_id: params[:post_id])
-    if like
-      puts 'in if'
-      puts like[:user_id]
-      if like[:user_id] == @user[:id]
-        puts 'in if if'
-        like.destroy
-        # user_no_like = {user: @user}
-        # render json: user_no_like
-        @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
-        post_arr = @posts.map do |po|
-          result = po.attributes
-          result[:username] = po.user.username
-          result[:reply] = po.replies
-          result[:like] = po.likes
-          result[:tags] = po.tags
-          result[:building] = params[:building_id]
-          result
-        end
-        puts post_arr
-        render json: post_arr
-      else
-        @like = @user.likes.create!(
-          post_id: params[:post_id]
-        )
-        # puts '@like'
-        # puts @like.inspect
-        # user_like = {user: @user, like: @like}
-        # puts 'user_like'
-        # puts user_like
-        # render json: user_like
-        @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
-        post_arr = @posts.map do |po|
-          result = po.attributes
-          result[:username] = po.user.username
-          result[:reply] = po.replies
-          result[:like] = po.likes
-          result[:tags] = po.tags
-          result[:building] = params[:building_id]
-          result
+    like = Like.where(post_id: params[:post_id])
+    puts like.size
+    if like.size > 0
+      puts 'IN IF'
+      # puts like[:user_id]
+      like.each do |e|
+        puts e[:user_id]
+        if e[:user_id] == @user[:id]
+          puts 'IN IF IF'
+
+          puts 'e[:user_id]'
+          puts e[:user_id]
+          puts '@user[:id]'
+          puts @user[:id]
+          e.destroy
+          @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
+          post_arr = @posts.map do |po|
+            result = po.attributes
+            result[:username] = po.user.username
+            result[:reply] = po.replies
+            result[:like] = po.likes
+            result[:tags] = po.tags
+            result[:building] = params[:building_id]
+            result
           end
-          puts 'post_arr'
           puts post_arr
-        render json: post_arr
+          render json: post_arr
+          break
+        else
+          puts 'IN ELSE'
+          @like = @user.likes.create!(
+            post_id: params[:post_id]
+          )
+          @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
+          post_arr = @posts.map do |po|
+            result = po.attributes
+            result[:username] = po.user.username
+            result[:reply] = po.replies
+            result[:like] = po.likes
+            result[:tags] = po.tags
+            result[:building] = params[:building_id]
+            result
+            end
+            puts 'post_arr'
+            puts post_arr
+          render json: post_arr
+        end
       end
+
+
+
+
+      # if like[:user_id] == @user[:id]
+      #   puts 'IN IF IF'
+      #   like.destroy
+      #   # user_no_like = {user: @user}
+      #   # render json: user_no_like
+      #   @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
+      #   post_arr = @posts.map do |po|
+      #     result = po.attributes
+      #     result[:username] = po.user.username
+      #     result[:reply] = po.replies
+      #     result[:like] = po.likes
+      #     result[:tags] = po.tags
+      #     result[:building] = params[:building_id]
+      #     result
+      #   end
+      #   puts post_arr
+      #   render json: post_arr
+      # else
+      #   puts 'IN ELSE'
+      #   @like = @user.likes.create!(
+      #     post_id: params[:post_id]
+      #   )
+      #   # puts '@like'
+      #   # puts @like.inspect
+      #   # user_like = {user: @user, like: @like}
+      #   # puts 'user_like'
+      #   # puts user_like
+      #   # render json: user_like
+      #   @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
+      #   post_arr = @posts.map do |po|
+      #     result = po.attributes
+      #     result[:username] = po.user.username
+      #     result[:reply] = po.replies
+      #     result[:like] = po.likes
+      #     result[:tags] = po.tags
+      #     result[:building] = params[:building_id]
+      #     result
+      #     end
+      #     puts 'post_arr'
+      #     puts post_arr
+      #   render json: post_arr
+      # end
     else
       puts 'in else'
       @like = @user.likes.create!(
         post_id: params[:post_id]
       )
-      # puts '@like'
-      # puts @like.inspect
-      # user_like = {user: @user, like: @like}
-      # puts 'user_like'
-      # puts user_like
-      # render json: user_like
       @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
       post_arr = @posts.map do |po|
         result = po.attributes
@@ -89,12 +132,3 @@ class LikesController < ApplicationController
     puts 'LIKES/CREATE/DESTROY CONTROLLER OUT'
   end
 end
-
-  # def destroy
-  #   puts params[:likes]
-  #   puts params
-  #   puts 'LIKES/DESTROY CONTROLLER IN'
-  #   @like = Like.find_by(id: params[:likeId])
-  #   @like.delete()
-  #   puts 'LIKES/DESTROY CONTROLLER OUT'
-  # end
