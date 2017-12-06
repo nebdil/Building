f9c061ae2bf05d6ed17378fb4c0748e01ddf627c
nebdil <dilannebioglu@gmail.com>
2017-12-06 12:25:33 -0500

    cleared comments

--- building-react/src/Tag.jsx
+++ building-react/src/Tag.jsx
@@ -10,24 +10,13 @@ export default class Tag extends Component {
   render() {
     let newArr = []
     this.props.posts.map(function(e){
-    //   if (e.tags) {
-    //     console.log(e.tags)
-    //     e.tags.map(function(a) {
-    //       newArr.push(a.name)
-    //       // console.log("a.name " + a.name)
-    //     })
-      // }
-    //   // console.log(e.tags)
       e.tags.forEach(function(a) {
         newArr.push(a.name)
-    //   //   // console.log("a.name " + a.name)
       })
     })
-    // // console.log("newArr " + newArr)
     let unique = [...new Set(newArr)];
 
     let func = this.props.handlePostsByTags
-    // let unique = this.props.unique_tags
     let selected = this.props.selectedTag
     let btnClass = this.props.btnClass
 
