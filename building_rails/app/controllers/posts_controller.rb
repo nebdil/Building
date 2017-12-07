ea17552b567eb39b5515427b34be39d8368d27d4
mcemolcusenler <cem.olcusenler@gmail.com>
2017-12-07 04:48:20 +0000

    fixes the hard-coded values in posts#index

--- building_rails/app/controllers/posts_controller.rb
+++ building_rails/app/controllers/posts_controller.rb
@@ -3,14 +3,14 @@ class PostsController < ApplicationController
 
   def index
     puts "post#index in"
-    @posts = Post.joins(:user).includes(:user).where(users: {building_id: 1}).order('posts.id DESC')
+    @posts = Post.joins(:user).includes(:user).where(users: {building_id: params[:building_id]}).order('posts.id DESC')
     post_arr = @posts.map do |po|
       result = po.attributes
       result[:username] = po.user.username
       result[:reply] = po.replies
       result[:like] = po.likes
       result[:tags] = po.tags
-      result[:building] = 1
+      result[:building] = params[:building_id].to_i
       result
       p result
     end
